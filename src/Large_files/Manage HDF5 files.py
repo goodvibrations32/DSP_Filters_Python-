e9d6bb3c3ff8ce68039ed576754bb021e0dc6f5b
Nick Torosian <goodvibrations32@vivaldi.net>
2022-03-06 16:42:14 +0200

    Print the labels of the datasets

--- src/Large_files/Manage HDF5 files.py
+++ src/Large_files/Manage HDF5 files.py
@@ -1,8 +1,9 @@
 #%%
 # find out how many sets of data are in the file
 import h5py
-f = h5py.File('/home/goodvibrationskde/Documents/Large_files/hdf5_sample_data.h5', 'r')
+f = h5py.File('/home/goodvibrationskde/Documents/hdf5_sample_data.h5', 'r')
 f.keys()
+print(f.keys())
 f.close()
 
 
