21f8168cb9660ac62cdef7096fcccbc48badc45c
goodvibrations32 <tm6220@edu.hmu.gr>
2022-04-02 01:39:19 +0300

    delete unwanted files

--- src/create_dataset_filt_high_res_.py
+++ src/create_dataset_filt_high_res_.py
@@ -31,16 +31,12 @@ for element2 in raw_sig_df:
     sig_array.append(np.array(element2))
 
 #%%
-#////////////////////////////////////////////////////////////////////////////////////////////////////////
-#////////////////////////////////////////////////////////////////////////////////////////////////////////
-#////////////////////////////////////////////////////////////////////////////////////////////////////////
 #The only filter with les standard deviation on output from input
-#Somehow should test more of these kind of filters with no window functions 
 
 #FIR Low-pass filter on the signal with the inverter connected and off
 
 #The length of the filter(number of coefficients, the filter order + 1)
-numtaps_2 = 2000
+numtaps_2 = 100_000
 fs = 500000
 cutoff_hz = 0.00001
 nyq_rate = fs/2 
@@ -94,7 +90,7 @@ for element4 in experiment:
 
 F = []
 for i in range(0,len(L)):
-    F.append(L[i].replace("raw","filt"))
+    F.append(L[i].replace("raw","HD"))
 i=0
 
 #%%
@@ -110,17 +106,16 @@ df.insert(loc=0, column="Time", value=time_no_shift)
 #and add the __NAME__.h5 at the end of the screen 
 # WARNING : If file already exists in the dir and it is closed via :
 #hf_st_pd_.close() it will be overwritten
-#final_data = [filt_con_off, filt_con_on, filt_con_on_WS_5, filt_discon_off,filt_discon_on,filt_discon_on_WS_5]
 
 #%%
-new_file_name = input("""Enter the name of the new folder : 
-the file will be created in the same path with the raw data file
+new_file_name = input("""Enter the name of the new file : 
+the file will be created in the same path folder with the raw data 
 if 0 is passed for a new name the old name is used with replacing
-the "raw" at the end of the name with "filt"
+the "raw" at the start of the name with "HD"
 """)
 
 if new_file_name == '0' :
-    file_name = file_name_of_raw.replace("raw", "filt")
+    file_name = file_name_of_raw.replace("raw", "HD")
 else:
     file_name = new_file_name
 
